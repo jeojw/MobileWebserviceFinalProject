15dac600db0a728d45df1dd930b085c733cc331f
jeojw <53222593+jeojw@users.noreply.github.com>
2025-12-19 23:27:59 +0900

    오류 수정

--- android/app/src/main/java/com/example/myapplication/api/RetrofitInstance.java
+++ android/app/src/main/java/com/example/myapplication/api/RetrofitInstance.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://your_pythonanywhere.com/api/";
+    private static final String BASE_URL = "https://localhost:8000/upload/";
 
     public static ApiService getApi() {
         if (retrofit == null) {
